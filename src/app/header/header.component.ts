0996f479ccb29d36aac9627b91658d336ce5a68a
Arjan Singh Bal <arjan3141@gmail.com>
2020-06-28 12:28:42 +0530

    added link to github repo in navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent { }
